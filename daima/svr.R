bb91c2dbae41c50b143b80b4340cc29688cc952e
yiii777888 <164477025+yiii777888@users.noreply.github.com>
2026-05-03 22:18:51 +0800

    Update svr.R

--- daima/svr.R
+++ daima/svr.R
@@ -1,7 +1,3 @@
-library(caret)
-preProc <- preProcess(train_data1, method = c("center", "scale"))
-train_scaled <- predict(preProc, train_data1)
-test_scaled <- predict(preProc, test_data1)
 tuneGrid <- expand.grid(
     sigma = 2^seq(-15, 3, length.out = 5),  
     C = 2^seq(-5, 15, length.out = 5)       
@@ -11,9 +7,9 @@ ctrl <- trainControl(
   number = 5,
   verboseIter = TRUE
 )
-set.seed(123)
+
 svr_model <- train(
-    X ~ Y_adj7,                
+    X ~ Y,                
     data = train_scaled,  
     method = "svmRadial",
     tuneGrid = tuneGrid,
