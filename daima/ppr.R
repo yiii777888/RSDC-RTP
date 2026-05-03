1d35ab3a51fef955439b60e390f8a0f96c03dde6
yiii777888 <164477025+yiii777888@users.noreply.github.com>
2026-05-03 22:17:50 +0800

    Update ppr.R

--- daima/ppr.R
+++ daima/ppr.R
@@ -1,11 +1,10 @@
-require(stats, quietly = TRUE)
-set.seed(123)  # 确保可重复性
+
 find_best_nterms <- function(data, response_var = "X", max_terms = 5) {
     best_nterms <- 1
     best_mse <- Inf     
     for(nterms in 1:max_terms) {
         model <- tryCatch({
-            ppr(as.formula(paste(response_var, "~ Y_adj6")), 
+            ppr(as.formula(paste(response_var, "~ Y")), 
                 data = data,
                 nterms = nterms,
                 max.terms = max_terms)  
@@ -24,8 +23,8 @@ find_best_nterms <- function(data, response_var = "X", max_terms = 5) {
 }
 optimal_nterms <- find_best_nterms(train_data1, "X", max_terms = 5)
 cat("\n最佳投影寻踪项数:", optimal_nterms, "\n")
-ppr_model <- ppr(X ~ Y_adj6,
-                 data = train_data1,
+ppr_model <- ppr(X ~ Y,
+                 data = train_data,
                  nterms = optimal_nterms,
                  max.terms = optimal_nterms + 2)
-predictions <- predict(ppr_model, test_data1)
+predictions <- predict(ppr_model, test_data)
