c8ecc5c29e68a1fabacc8abfc1448be5256a2f8b
yiii777888 <164477025+yiii777888@users.noreply.github.com>
2026-05-05 16:43:55 +0800

    Update brnn.R

--- daima/brnn.R
+++ daima/brnn.R
@@ -3,9 +3,5 @@ brnn_model <- brnn(Y ~ X,
                    data = train_data,
                    normalize = TRUE,
                    neurons = val_par1,
-                   verbose = TRUE,
-                   mu = 0.01,      
-                   mu_dec = 0.1,
-                   mu_inc = 10,
-                   mu_max = 1e10)
+                   verbose = TRUE,)
 predictions <- predict(brnn_model, test_data)
