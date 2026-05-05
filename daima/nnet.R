5474a9cb550009d6744ced2316f5faeee0bf98ad
yiii777888 <164477025+yiii777888@users.noreply.github.com>
2026-05-05 16:32:27 +0800

    Update nnet.R

--- daima/nnet.R
+++ daima/nnet.R
@@ -1,18 +1,12 @@
-ctrl <- trainControl(
-    method = "cv",      
-    number = 5,        
-    verboseIter = TRUE  
-) 
+
 
 nnet.model <- train(
     X ~ Y,              
     data = train_data,
     method = "nnet",    
     tuneGrid = tuneGrid,
-    trControl = ctrl,
     trace = FALSE,      
     MaxNWts = 10000,    
-    linout = TRUE,     
-    metric = "RMSE"     )
+    linout = TRUE    )
 
 predictions <- predict(nnet.model, newdata = test_data)
