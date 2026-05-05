85a6408b6f2d7cbbc0de976b2ca3053d8139a205
yiii777888 <164477025+yiii777888@users.noreply.github.com>
2026-05-05 16:42:00 +0800

    Update svr.R

--- daima/svr.R
+++ daima/svr.R
@@ -1,21 +1,9 @@
-tuneGrid <- expand.grid(
-    sigma = 2^seq(-15, 3, length.out = 5),  
-    C = 2^seq(-5, 15, length.out = 5)       
-)
-ctrl <- trainControl(
-  method = "cv",
-  number = 5,
-  verboseIter = TRUE
-)
 
 svr_model <- train(
-    X ~ Y,                
+    Y ~ X,                
     data = train_scaled,  
     method = "svmRadial",
-    tuneGrid = tuneGrid,
-    trControl = ctrl,
-    metric = "RMSE"       
-)
+    tuneGrid = tuneGrid )
 
 pred_scaled <- predict(svr_model, newdata = test_scaled)
 predictions <- pred_scaled * preProc$std[["X"]] + preProc$mean[["X"]]
