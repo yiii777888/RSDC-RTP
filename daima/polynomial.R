d9e306a02f4e6de8f652db3241e70a9e27992370
yiii777888 <164477025+yiii777888@users.noreply.github.com>
2026-05-05 16:37:18 +0800

    Update polynomial.R

--- daima/polynomial.R
+++ daima/polynomial.R
@@ -2,36 +2,5 @@
     model <- lm(y ~ poly(x, degree, raw = TRUE))
     return(model)
 }
-find_best_degree <- function(x, y, max_degree = 5) {
-    best_degree <- 1
-    best_rmse <- Inf    
-    
-    degree_seq <- seq(0.5, max_degree, by = 0.5)    
-
-    folds <- sample(rep(1:5, length.out = length(x)))     
-    for(degree in degree_seq) {
-        cv_errors <- numeric(5)        
-        for(fold in 1:5) {
-            train_idx <- which(folds != fold)
-            test_idx <- which(folds == fold)            
-            model <- tryCatch({
-                poly_reg(x[train_idx], y[train_idx], degree)
-            }, error = function(e) NULL)            
-            if(!is.null(model)) {
-                pred <- predict(model, data.frame(x = x[test_idx]))
-                cv_errors[fold] <- sqrt(mean((y[test_idx] - pred)^2))
-            } else {
-                cv_errors[fold] <- Inf
-            }
-        }        
-        current_rmse <- mean(cv_errors)        
-        if(current_rmse < best_rmse) {
-            best_rmse <- current_rmse
-            best_degree <- degree
-        }
-    }    
-    return(best_degree)
-}
-optimal_degree <- find_best_degree(train_set$Y, train_set$X, max_degree = 5)
-final_model <- poly_reg(train_set$Y, train_set$X, optimal_degree)
-valid_predictions <- predict(final_model, data.frame(x = valid_set$Y))
+final_model <- poly_reg(train_set$X, train_set$Y, val_par1)
+valid_predictions <- predict(final_model, data.frame(x = valid_set$X))
